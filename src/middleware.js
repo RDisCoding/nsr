0417fd384b1e54d8a11ffbf6791d502b1d4ae5ea
Smitvd22 <smitvdeoghare@gmail.com>
2025-02-02 08:18:58 +0530

    Rudray's success

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,23 @@
-import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export default withAuth({
-  pages: { signIn: '/login' },
-});
+export async function middleware(req) {
+  const { pathname } = req.nextUrl;
+  if (
+    pathname === "/" ||
+    pathname.startsWith("/api/auth") ||
+    pathname.startsWith("/_next") ||
+    pathname.includes("favicon.ico")
+  ) {
+    return NextResponse.next();
+  }
+  const token = await getToken({ req });
+  if (!token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  return NextResponse.next();
+}
 
-export const config = { matcher: ['/protected/:path*'] };
+export const config = {
+  matcher: ["/((?!api/auth|_next/static|favicon.ico).*)"],
+};
